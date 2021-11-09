0be3b910c3a3f579e943130884ecfb84743f9d7c
taiyounari@gmail.com <49590699+taiyounari@users.noreply.github.com>
2021-11-09 10:00:39 +0700

    Fix for danh

--- backend/src/controllers/forum/category.controller.js
+++ backend/src/controllers/forum/category.controller.js
@@ -1,13 +1,13 @@
 const db = require("../../models");
 
-const categoryController = db.categoryController;
+const ForumCategory = db.forumCategory;
 const Category = require('../../models/forum/Category');
 const Topic = require('../../models/forum/Topic');
 const Post = require('../../models/forum/Post');
 
 
-// retrieve all categories with all info
-categoryController.get('/', (req, res) => {
+// retrieve all categories with all info /get '/'
+exports.getAll = (req, res) => {
     Category.find()
         .lean()
         .populate({
@@ -35,8 +35,8 @@ categoryController.get('/', (req, res) => {
                     category.subcategories,
                     (subcategory, callback) => {
                         Topic.countDocuments({
-                                subcategory: subcategory._id,
-                            })
+                            subcategory: subcategory._id,
+                        })
                             .then(topicCount => {
                                 subcategory.topics = topicCount;
                                 subcategory.posts = 0;
@@ -67,66 +67,58 @@ categoryController.get('/', (req, res) => {
                 );
             });
         });
-});
+};
 
 
-// get info from a single category
-categoryController.get('/:id', (req, res) => {
+// get info from a single category get(/:id)
+exports.getById = (req, res) => {
     Category.findById(req.params.id)
         .then(category => res.status(200).json({ category }))
         .catch(err =>
             res.status(400).json({ msg: 'Failed to get info of category', err })
         );
-});
+};
 
 
-// add a new category
-categoryController.post(
-    '/add',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        const { name } = req.body;
-        const newCategory = new Category({
-            name,
-        });
+// add a new category post('/add',passport.authenticate('jwt', { session: false })
+exports.addNewCategory = (req, res) => {
+    const { name } = req.body;
+    const newCategory = new Category({
+        name,
+    });
 
-        newCategory
-            .save()
-            .then(category => res.status(200).json({ category }))
-            .catch(err => res.json({ msg: 'Failed to add a new category', err }));
-    }
-);
+    newCategory
+        .save()
+        .then(category => res.status(200).json({ category }))
+        .catch(err => res.json({ msg: 'Failed to add a new category', err }));
+}
 
 
-// update a category
-categoryController.post(
-    '/update',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        Category.findByIdAndUpdate(req.body.id, req.body, {
-                useFindAndModify: false,
-            })
-            .then(category =>
-                res.status(200).json({ msg: 'Category updated', category })
-            )
-            .catch(err =>
-                res.status(400).json({ msg: 'Failed to update category', err })
-            );
-    }
-);
+
+// update a category post('/update',passport.authenticate('jwt', { session: false })
+exports.update = (req, res) => {
+    Category.findByIdAndUpdate(req.body.id, req.body, {
+        useFindAndModify: false,
+    })
+        .then(category =>
+            res.status(200).json({ msg: 'Category updated', category })
+        )
+        .catch(err =>
+            res.status(400).json({ msg: 'Failed to update category', err })
+        );
+}
 
 
-// delete a category
-categoryController.post(
-    '/delete',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        Category.findByIdAndDelete(req.body.id)
-            .then(() => res.status(200).json({ msg: 'Category deleted' }))
-            .catch(err =>
-                res.status(400).json({ msg: 'Failed to delete category', err })
-            );
-    }
-);
 
-module.exports = categoryController;
+// delete a category post('/delete',passport.authenticate('jwt', { session: false }))
+//-> delete('/delete',passport.authenticate('jwt', { session: false })) at router 
+exports.delete = (req, res) => {
+    Category.findByIdAndDelete(req.body.id)
+        .then(() => res.status(200).json({ msg: 'Category deleted' }))
+        .catch(err =>
+            res.status(400).json({ msg: 'Failed to delete category', err })
+        );
+}
+
+
+module.exports = exports.create;
